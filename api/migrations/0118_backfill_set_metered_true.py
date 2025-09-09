4af7e07b4870cf4e39e54e784b07aa9c1b280d45
Will <will@forwardbounding.com>
2025-09-09 15:35:18 -0400

    adding metered table and backfilling old records

---
+++ api/migrations/0118_backfill_set_metered_true.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+from django.utils import timezone
+
+
+def mark_existing_as_metered(apps, schema_editor):
+    BrowserUseAgentTask = apps.get_model('api', 'BrowserUseAgentTask')
+    PersistentAgentStep = apps.get_model('api', 'PersistentAgentStep')
+    # Mark all existing rows as metered to avoid double billing on first rollup
+    BrowserUseAgentTask.objects.filter(metered=False).update(metered=True)
+    PersistentAgentStep.objects.filter(metered=False).update(metered=True)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("api", "0117_add_metered_flags"),
+    ]
+
+    operations = [
+        migrations.RunPython(mark_existing_as_metered, migrations.RunPython.noop),
+    ]
+
