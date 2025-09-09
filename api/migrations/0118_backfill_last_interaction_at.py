58aad6725341867f14e4e0c5fa7d5968b82d308c
matt-greathouse <matt5316@gmail.com>
2025-09-09 13:20:52 -0400

    Backfill last_interaction_date

---
+++ api/migrations/0118_backfill_last_interaction_at.py
@@ -0,0 +1,24 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("api", "0117_alter_persistentagent_last_interaction_at"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql=(
+                "UPDATE api_persistentagent "
+                "SET last_interaction_at = COALESCE(last_interaction_at, created_at) "
+                "WHERE last_interaction_at IS NULL;"
+            ),
+            reverse_sql=(
+                "UPDATE api_persistentagent "
+                "SET last_interaction_at = NULL "
+                "WHERE last_interaction_at = created_at;"
+            ),
+        )
+    ]
+
