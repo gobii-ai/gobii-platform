61884c194a19cfd795f6664f6984f4e73234d5df
Will <will@forwardbounding.com>
2025-09-18 19:33:34 -0400

    WIP for plan configuration

--- constants/plans.py
+++ constants/plans.py
@@ -4,7 +4,14 @@ class PlanNames:
     FREE = "free"
     STARTUP = "startup"
 
+    # Org Plans
+    ORG_TEAM = "org_team"
+
+
 
 class PlanNamesChoices(models.TextChoices):
     FREE = PlanNames.FREE, "Free"
     STARTUP = PlanNames.STARTUP, "Startup"
+
+    # Org Plans
+    ORG_TEAM = PlanNames.ORG_TEAM, "Team"
