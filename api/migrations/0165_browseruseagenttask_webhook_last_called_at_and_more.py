754763cdb0d19badddf59bd0870f7c5ea733765f
matt-greathouse <matt5316@gmail.com>
2025-10-17 14:14:25 -0400

    Rename migration

--- api/migrations/0165_browseruseagenttask_webhook_last_called_at_and_more.py
+++ api/migrations/0165_browseruseagenttask_webhook_last_called_at_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0163_browsermodelendpoint_max_output_tokens'),
+        ('api', '0164_persistentagentwebhook'),
     ]
 
     operations = [
