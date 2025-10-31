c1cf95d1efc21b300ae6ad210cda6e1e04ab1c38
matt-greathouse <matt5316@gmail.com>
2025-10-31 09:58:23 -0400

    Fix migration

--- api/migrations/0174_persistentagent_tags_and_more.py
+++ api/migrations/0174_persistentagent_tags_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0172_mcp_oauth_models'),
+        ('api', '0173_persistentagent_mini_description'),
     ]
 
     operations = [
