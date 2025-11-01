ce06d7dc41b216f93bedd477b97cf60ad308eaca
Will <will@forwardbounding.com>
2025-11-01 13:44:18 -0400

    correcting tokens etc

--- marketing_events/providers/__init__.py
+++ marketing_events/providers/__init__.py
@@ -6,8 +6,8 @@ from .reddit import RedditCAPI
 
 def get_providers():
     providers = []
-    if getattr(settings, "META_CAPI_TOKEN", None) and getattr(settings, "META_PIXEL_ID", None):
-        providers.append(MetaCAPI(pixel_id=settings.META_PIXEL_ID, token=settings.META_CAPI_TOKEN))
-    if getattr(settings, "REDDIT_CONVERSIONS_TOKEN", None) and getattr(settings, "REDDIT_AD_ACCOUNT", None):
-        providers.append(RedditCAPI(ad_account=settings.REDDIT_AD_ACCOUNT, token=settings.REDDIT_CONVERSIONS_TOKEN))
+    if getattr(settings, "FACEBOOK_ACCESS_TOKEN", None) and getattr(settings, "META_PIXEL_ID", None):
+        providers.append(MetaCAPI(pixel_id=settings.META_PIXEL_ID, token=settings.FACEBOOK_ACCESS_TOKEN))
+    if getattr(settings, "REDDIT_ACCESS_TOKEN", None) and getattr(settings, "REDDIT_ADVERTISER_ID", None):
+        providers.append(RedditCAPI(ad_account=settings.REDDIT_ADVERTISER_ID, token=settings.REDDIT_ACCESS_TOKEN))
     return providers
