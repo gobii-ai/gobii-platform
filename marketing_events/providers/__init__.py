fbaca01fe7290376fd33666fc60b4c29a5c14ed7
Will <will@forwardbounding.com>
2025-11-01 17:21:52 -0400

    correcting reddit init params

--- marketing_events/providers/__init__.py
+++ marketing_events/providers/__init__.py
@@ -9,5 +9,5 @@ def get_providers():
     if getattr(settings, "FACEBOOK_ACCESS_TOKEN", None) and getattr(settings, "META_PIXEL_ID", None):
         providers.append(MetaCAPI(pixel_id=settings.META_PIXEL_ID, token=settings.FACEBOOK_ACCESS_TOKEN))
     if getattr(settings, "REDDIT_ACCESS_TOKEN", None) and getattr(settings, "REDDIT_ADVERTISER_ID", None):
-        providers.append(RedditCAPI(ad_account=settings.REDDIT_ADVERTISER_ID, token=settings.REDDIT_ACCESS_TOKEN))
+        providers.append(RedditCAPI(pixel_id=settings.REDDIT_ADVERTISER_ID, token=settings.REDDIT_ACCESS_TOKEN))
     return providers
