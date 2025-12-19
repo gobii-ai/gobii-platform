110b20c0e37f3311c97097e406ed95fee19130ff
matt-greathouse <matt5316@gmail.com>
2025-12-19 15:07:01 -0500

    Fix display on dev sites

--- config/urls.py
+++ config/urls.py
@@ -376,3 +376,4 @@ urlpatterns.insert(1, path("", include(("proprietary.urls", "proprietary"), name
 # Serve static files in development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
