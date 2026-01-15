4af4c7c8fdf28195c9a388571e8e816804f66e4c
Will <will@forwardbounding.com>
2026-01-14 21:41:25 -0500

    adding segment analytics for agent chat

--- middleware/app_shell.py
+++ middleware/app_shell.py
@@ -35,8 +35,36 @@ def _format_vite_tags() -> str:
     return "\n".join(tags)
 
 
+def _format_segment_snippet() -> str:
+    """Generate Segment analytics snippet if configured."""
+    write_key = getattr(settings, "SEGMENT_WEB_WRITE_KEY", None)
+    if not write_key:
+        # Provide a no-op stub in debug mode or when not configured
+        return """<script>
+    window.analytics = window.analytics || {
+      page: function() {},
+      track: function() {},
+      identify: function() {},
+      ready: function(cb) { if (typeof cb === 'function') cb(); },
+    };
+  </script>"""
+
+    return f"""<script>
+    !function(){{var i="analytics",analytics=window[i]=window[i]||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","screen","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware","register"];analytics.factory=function(e){{return function(){{if(window[i].initialized)return window[i][e].apply(window[i],arguments);var n=Array.prototype.slice.call(arguments);if(["track","screen","alias","group","page","identify"].indexOf(e)>-1){{var c=document.querySelector("link[rel='canonical']");n.push({{__t:"bpc",c:c&&c.getAttribute("href")||void 0,p:location.pathname,u:location.href,s:location.search,t:document.title,r:document.referrer}})}}n.unshift(e);analytics.push(n);return analytics}}}};for(var n=0;n<analytics.methods.length;n++){{var key=analytics.methods[n];analytics[key]=analytics.factory(key)}}analytics.load=function(key,n){{var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.setAttribute("data-global-segment-analytics-key",i);t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r);analytics._loadOptions=n}};analytics.SNIPPET_VERSION="5.2.0";}}}}();
+    analytics.load("{write_key}");
+    analytics.addSourceMiddleware(({{ payload, next }}) => {{
+      if (!payload.obj.properties) payload.obj.properties = {{}};
+      payload.obj.properties.medium = 'Web';
+      payload.obj.properties.frontend = true;
+      next(payload);
+    }});
+    analytics.page('App', 'Immersive App');
+  </script>"""
+
+
 def _build_shell_html() -> str:
     vite_tags = _format_vite_tags()
+    segment_snippet = _format_segment_snippet()
     icon_url = static("images/noBgBlue.png")
     fonts_css = static("css/custom_fonts.css")
     pygments_css = static("css/pygments.css")
@@ -53,6 +81,7 @@ def _build_shell_html() -> str:
   <link rel="stylesheet" href="{fonts_css}">
   <link rel="stylesheet" href="{pygments_css}">
   <link rel="stylesheet" href="{globals_css}">
+  {segment_snippet}
   {vite_tags}
 </head>
 <body class="min-h-screen bg-white">
