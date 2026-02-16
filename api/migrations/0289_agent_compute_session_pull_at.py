81f437c1368aaeb8fc01df527965ef1196bd17fc
matt-greathouse <matt5316@gmail.com>
2026-02-16 13:15:17 -0500

    Fix migration

--- api/migrations/0289_agent_compute_session_pull_at.py
+++ api/migrations/0289_agent_compute_session_pull_at.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("api", "0286_intelligencetier_is_default"),
+        ("api", "0288_merge_20260211_1743"),
     ]
 
     operations = [
