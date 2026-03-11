cabafb976a1187106b6b2197bb6704500f2ebc0e
matt-greathouse <matt5316@gmail.com>
2026-03-11 13:11:48 -0400

    fix migration

--- api/migrations/0315_persistentagenthumaninputrequest_and_more.py
+++ api/migrations/0315_persistentagenthumaninputrequest_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0313_add_owner_execution_pause_switches'),
+        ('api', '0314_pipedreamappselection'),
     ]
 
     operations = [
