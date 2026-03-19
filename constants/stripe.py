a1785ae1b9ec9ce4bf8bf688903483bbbbd20d24
Will <will@forwardbounding.com>
2026-03-19 09:48:30 -0400

    switch to an exclusion list for payment types vs inclusion

--- constants/stripe.py
+++ constants/stripe.py
@@ -1,6 +1,7 @@
 """Shared Stripe-related constants."""
 
 CHECKOUT_PAYMENT_METHOD_TYPES = ["card", "link"]
+EXCLUDED_PAYMENT_METHOD_TYPES = ["cashapp", "klarna"]
 
 ORG_OVERAGE_STATE_META_KEY = "org_overage_sku_state"
 ORG_OVERAGE_STATE_DETACHED_PENDING = "detached_pending"
