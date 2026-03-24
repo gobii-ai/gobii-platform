32681f30ad463be53ad4867591bab55c83ce9a61
matt-greathouse <matt5316@gmail.com>
2026-03-24 11:39:35 -0400

    fix migration

--- api/migrations/0327_persistentagentinboundwebhook.py
+++ api/migrations/0327_persistentagentinboundwebhook.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0324_add_user_trial_eligibility_enforcement_flag'),
+        ('api', '0326_decodoipblock_proxy_type'),
     ]
 
     operations = [
