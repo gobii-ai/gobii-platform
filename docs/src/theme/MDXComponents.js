03ce30cc0de6224e108f8e866c8425cb59022b75
Andrew I. Christianson <andrew@Andrews-MacBook-Pro-2.local>
2026-05-16 19:08:46 -0400

    Add product screenshots to docs

--- docs/src/theme/MDXComponents.js
+++ docs/src/theme/MDXComponents.js
@@ -5,7 +5,17 @@ function Card({children}) {
   return <div className="gobii-doc-card">{children}</div>;
 }
 
+function Screenshot({src, alt, caption, children}) {
+  return (
+    <figure className="gobii-doc-screenshot">
+      <img src={src} alt={alt} loading="lazy" />
+      {caption || children ? <figcaption>{caption || children}</figcaption> : null}
+    </figure>
+  );
+}
+
 export default {
   ...MDXComponents,
   Card,
+  Screenshot,
 };
