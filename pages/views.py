8e7cd175991454747bfa09a5235864a4bafde3f8
Will <will@forwardbounding.com>
2026-05-21 09:01:20 -0400

    optional suppression of htmx if a page doesnt need it

--- pages/views.py
+++ pages/views.py
@@ -964,6 +964,7 @@ class HomePage(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["suppress_htmx"] = True
         # Add agent charter form for the home page spawn functionality
         from console.forms import PersistentAgentCharterForm
 
