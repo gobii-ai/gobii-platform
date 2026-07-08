61ad490c3ac14a4f12d4008afacc077ebaeb9ab6
Will <will@forwardbounding.com>
2026-07-08 09:56:43 -0400

    merge migrations; complexity reduction

--- api/storage_backends.py
+++ api/storage_backends.py
@@ -11,53 +11,10 @@ class AliasedStorage(Storage):
         self.alias = alias
 
     @property
-    def _wrapped_storage(self):
+    def _storage(self):
         return storages[self.alias]
 
-    def open(self, name, mode="rb"):
-        return self._wrapped_storage.open(name, mode)
-
-    def save(self, name, content, max_length=None):
-        return self._wrapped_storage.save(name, content, max_length=max_length)
-
-    def delete(self, name):
-        return self._wrapped_storage.delete(name)
-
-    def exists(self, name):
-        return self._wrapped_storage.exists(name)
-
-    def listdir(self, path):
-        return self._wrapped_storage.listdir(path)
-
-    def size(self, name):
-        return self._wrapped_storage.size(name)
-
-    def url(self, name):
-        return self._wrapped_storage.url(name)
-
-    def path(self, name):
-        return self._wrapped_storage.path(name)
-
-    def get_accessed_time(self, name):
-        return self._wrapped_storage.get_accessed_time(name)
-
-    def get_created_time(self, name):
-        return self._wrapped_storage.get_created_time(name)
-
-    def get_modified_time(self, name):
-        return self._wrapped_storage.get_modified_time(name)
-
-    def get_valid_name(self, name):
-        return self._wrapped_storage.get_valid_name(name)
-
-    def get_alternative_name(self, file_root, file_ext):
-        return self._wrapped_storage.get_alternative_name(file_root, file_ext)
-
-    def get_available_name(self, name, max_length=None):
-        return self._wrapped_storage.get_available_name(name, max_length=max_length)
-
-    def generate_filename(self, filename):
-        return self._wrapped_storage.generate_filename(filename)
-
-    def __getattr__(self, name):
-        return getattr(self._wrapped_storage, name)
+    def __getattribute__(self, name):
+        if name.startswith("__") or name in {"alias", "_storage", "deconstruct", "_constructor_args"}:
+            return super().__getattribute__(name)
+        return getattr(super().__getattribute__("_storage"), name)
