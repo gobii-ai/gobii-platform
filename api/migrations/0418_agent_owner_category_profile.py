bb0c3ae95f732fa6590080b6421f02d5bb8fce43
matt-greathouse <matt5316@gmail.com>
2026-07-09 13:18:14 -0400

    Fix migration

--- api/migrations/0418_agent_owner_category_profile.py
+++ api/migrations/0418_agent_owner_category_profile.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("api", "0416_merge_20260708_1356"),
+        ("api", "0417_persistentagentuseractionevent"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
